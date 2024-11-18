173056d82496eab5e5865cd2dacde0fee92db8cd
cadin2 <cdsantos2015@gmail.com>
2024-11-18 11:47:16 -0300

    aula4
    listas

---
+++ aula4 - lista/exercicio1.py
@@ -0,0 +1,21 @@
+lista = [5, 8, 2, 9, 1]
+
+lista.sort()
+print(lista)
+
+lista.reverse()
+print(lista)
+
+lista.append(7)
+print(lista)
+
+lista.insert(2, 3)
+print(lista)
+
+lista[5] = 10
+print(lista)
+
+lista.remove(lista[0])
+print(lista)
+
+del lista
